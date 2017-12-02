62f87502fe1d212f46e495fb060f624a6bb859e0
Puteulanus <mj19941230@gmail.com>
2017-12-02 23:18:36 +0800

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ ! -f "/root/.ssh/id_rsa.pub" ];then
+  echo "Creating SSH Key..."
+  ssh-keygen -t rsa -b 4096 -C "c9ssh@delete.later" -N '' -f /root/.ssh/id_rsa -q
+  cat /root/.ssh/id_rsa.pub >> /root/.ssh/authorized_keys
+  REMOTE_IP=$(route -n | awk '/UG[ \t]/{print $2}')
+  ssh -o StrictHostKeyChecking=no ${SSH_USER:-root}@$REMOTE_IP echo
+  sed -i "s/REMOTE_IP_ADDR/$REMOTE_IP/"
+fi
+
+/root/.c9/node/bin/node server.js -l $C9_IP -p $C9_PORT -w $WORKSPACE_DIR -a $USERNAME:$PASSWORD --packed 
