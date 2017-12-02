ea9c522f8a7686b814839dc9ea649d8901e6483f
Puteulanus <mj19941230@gmail.com>
2017-12-02 23:54:59 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -6,7 +6,7 @@ if [ ! -f "/root/.ssh/id_rsa.pub" ];then
   cat /root/.ssh/id_rsa.pub >> /root/.ssh/authorized_keys
   REMOTE_IP=$(route -n | awk '/UG[ \t]/{print $2}')
   ssh -o StrictHostKeyChecking=no ${SSH_USER:-root}@$REMOTE_IP echo
-  sed -i "s/REMOTE_IP_ADDR/$REMOTE_IP/"
+  sed -i "s/REMOTE_IP_ADDR/$REMOTE_IP/" /usr/src/c9sdk/node_modules/vfs-local/localfs.js
 fi
 
 /root/.c9/node/bin/node server.js -l $C9_IP -p $C9_PORT -w $WORKSPACE_DIR -a $USERNAME:$PASSWORD --packed 
